6c2a6eb08f5ab1493152a3a50aee7254d1ec4f85
Lucas Ritzdorf <lucas.ritzdorf@gmail.com>
2018-10-09 20:29:41 -0600

    Create RobotC development branch, add skeleton code

---
+++ RobotCode.c
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------
+Robot code for GHS VEX Team G
+2018-19 Competiton Season
+
+Team members are Lucas Ritzdorf, Riker Foster, and Lauryn Vornbrock.
+---------------------------------------------------------------------------*/
+
+// Set up motors and remote here
+
+// Standard sleep value (in milliseconds)
+// Can be adjusted for responsiveness as needed
+const int sleepValue = 20
+
+task pre_drive() {
+	// Pre-driving setup
+	// Set servo positions, etc.
+}
+
+task autonomous() {
+	// Autonomous code
+}
+
+task driverControl() {
+	// Driver-control code
+}
+
+task main() {
+	pre_drive();
+}
